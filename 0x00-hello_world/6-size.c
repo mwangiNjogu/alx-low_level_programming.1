7e4515b863b6ebdfd3b9cd1c8e519091312efed8
mwangiNjogu <njogueustace2@gmail.com>
2022-12-08 03:59:54 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,25 +7,13 @@
 int main(void)
 
 {
-
 	  printf("Type char: %lu byte(s)\n", sizeof(char));
-
-	    printf("Type int: %lu byte(s)\n", sizeof(int));
-
-	      printf("Type long int: %lu byte(s)\n", sizeof(long int));
-
-	        printf("Type long long int: %lu byte(s)\n", sizeof(long long int));
-
-		  printf("Type float: %lu byte(s)\n", sizeof(float));
-
-		    printf("Type double: %lu byte(s)\n", sizeof(double));
-
-		      printf("Type long double: %lu byte(s)\n", sizeof(long double));
-
-		        printf("Type pointer: %lu byte(s)\n", sizeof(void *));
-
-			  return (0);
-
+      	  printf("Type int: %lu byte(s)\n", sizeof(int));
+  	  printf("Type long int: %lu byte(s)\n", sizeof(long int));
+      	  printf("Type long long int: %lu byte(s)\n", sizeof(long long int));
+	  printf("Type float: %lu byte(s)\n", sizeof(float));
+	  printf("Type double: %lu byte(s)\n", sizeof(double));
+	  return (0);
 }
 
 
