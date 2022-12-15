d6f32c97a9ac6908b0fed873d85694529ce0f7de
mwangiNjogu <njogueustace2@gmail.com>
2022-12-15 12:59:43 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+nclude "main.h"
+/**
+ * mul - multiplies two intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: addition of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
