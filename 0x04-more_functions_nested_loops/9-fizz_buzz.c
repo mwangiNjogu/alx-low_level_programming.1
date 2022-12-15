47ddb28bbb1da84556365ad4b9e044793d35aaae
mwangiNjogu <njogueustace2@gmail.com>
2022-12-15 13:47:43 -0800

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,22 +3,14 @@
 #include "main.h"
 
 /**
+ * main - FizzBuzz
+ * Description: prints the numbers 1 - 100 w/
  *
- * * main - FizzBuzz
+ * Fizz for multiples of 3, Buzz for multiples of 5,
  *
- * *
- *
- * * Description: prints the numbers 1 - 100 w/
- *
- * * Fizz for multiples of 3, Buzz for multiples of 5,
- *
- * * and FizzBuzz for multiples of both
- *
- * *
- *
- * * Return: always 0
- *
- * */
+ * and FizzBuzz for multiples of both
+ * Return: always 0
+ */
 
 int main(void)
 
