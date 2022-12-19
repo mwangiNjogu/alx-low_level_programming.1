858278b876907a8c12f4b5d2ba774fd8ee227b62
mwangiNjogu <njogueustace2@gmail.com>
2022-12-19 02:26:11 -0800

    task 5

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN-H
+#define MAIN_H
+
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,3 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
+
+#endif
