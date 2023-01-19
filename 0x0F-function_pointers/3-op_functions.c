c9113a72865e6280bb360bcebd85900554cd2d59
mwangiNjogu <njogueustace2@gmail.com>
2023-01-19 03:58:07 -0800

    all tasks

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+/** op_add - calculates the sume of two intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - calculates the differenc of two intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - calculates the product of two intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: prduct of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculates the division of two intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: result of the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates the remainder of the division of two intergers
+ * @: first interger
+ * @b: second interger
+ *
+ * Return: remaiinder of a divided b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
